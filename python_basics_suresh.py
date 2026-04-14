e2f62df1c6c486bf3c4242bbea8715879aaa2310
sureshsv625-hub <sureshsv625@gmail.com>
2026-04-14 18:12:03 +0530

    python_basics_suresh.py

---
+++ python_basics_suresh.py
@@ -0,0 +1,16 @@
+name = "Suresh"
+age = 35
+height = 5.6
+is_student = True
+print(name, age, height, is_student)
+print(f"Name: {name} | Age: {age} | Height: {height} | Student{is_student}")
+
+age_month = age * 12
+age_days = age * 365
+remainder_by_7 = age % 7
+age_squared = age**2
+
+print("Age in month:", age_month)
+print("age in days:", age_days)
+print("Remainder when age is divided by 7:", remainder_by_7)
+print("Age raised to the power of 2:", age_squared)
